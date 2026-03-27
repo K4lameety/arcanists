0e230c0a82ddb0febf1a7751abf7a459949f015e
K4lameety <rhkapota@gmail.com>
2026-03-28 03:17:40 +0700

    change space

--- app/contest/page.tsx
+++ app/contest/page.tsx
@@ -67,7 +67,7 @@ export default function ContestsPage() {
       <section className="relative w-full pt-4 pb-16 md:py-24 px-6 md:px-12 bg-black border-t border-neutral-900">
         <div className="max-w-7xl mx-auto relative z-10">
           <div className="mb-12 md:mb-16 space-y-3 md:space-y-4 text-left">
-            <h2 className="text-3xl md:text-7xl font-black text-white uppercase tracking-tighter">
+            <h2 className="text-3xl md:text-6xl font-black text-white uppercase tracking-tighter">
               Contests
             </h2>
             <div className="h-[2px] w-16 md:w-24 bg-orange-500 shadow-[0_0_10px_rgba(249,115,22,0.5)]"></div>
